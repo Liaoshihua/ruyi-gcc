4eb419561eb47ce6582444aa1603cd1177d48f77
Andre Vieira <andre.simoesdiasvieira@arm.com>
2023-06-05 17:53:34 +0100

    Remove widen_plus/minus_expr tree codes
    This patch removes the old widen plus/minus tree codes which have been
    replaced by internal functions.
    
    2023-06-05  Andre Vieira  <andre.simoesdiasvieira@arm.com>
    	    Joel Hutton  <joel.hutton@arm.com>
    
    gcc/ChangeLog:
    
    	* doc/generic.texi: Remove old tree codes.
    	* expr.cc (expand_expr_real_2): Remove old tree code cases.
    	* gimple-pretty-print.cc (dump_binary_rhs): Likewise.
    	* optabs-tree.cc (optab_for_tree_code): Likewise.
    	(supportable_half_widening_operation): Likewise.
    	* tree-cfg.cc (verify_gimple_assign_binary): Likewise.
    	* tree-inline.cc (estimate_operator_cost): Likewise.
    	(op_symbol_code): Likewise.
    	* tree-vect-data-refs.cc (vect_get_smallest_scalar_type): Likewise.
    	(vect_analyze_data_ref_accesses): Likewise.
    	* tree-vect-generic.cc (expand_vector_operations_1): Likewise.
    	* cfgexpand.cc (expand_debug_expr): Likewise.
    	* tree-vect-stmts.cc (vectorizable_conversion): Likewise.
    	(supportable_widening_operation): Likewise.
    	* gimple-range-op.cc (gimple_range_op_handler::maybe_non_standard):
    	Likewise.
    	* optabs.def (vec_widen_ssubl_hi_optab, vec_widen_ssubl_lo_optab,
    	vec_widen_saddl_hi_optab, vec_widen_saddl_lo_optab,
    	vec_widen_usubl_hi_optab, vec_widen_usubl_lo_optab,
    	vec_widen_uaddl_hi_optab, vec_widen_uaddl_lo_optab): Remove optabs.
    	* tree-pretty-print.cc (dump_generic_node): Remove tree code definition.
    	* tree.def (WIDEN_PLUS_EXPR, WIDEN_MINUS_EXPR, VEC_WIDEN_PLUS_HI_EXPR,
    	VEC_WIDEN_PLUS_LO_EXPR, VEC_WIDEN_MINUS_HI_EXPR,
    	VEC_WIDEN_MINUS_LO_EXPR): Likewise.

--- gcc/tree-cfg.cc
+++ gcc/tree-cfg.cc
@@ -4017,8 +4017,6 @@ verify_gimple_assign_binary (gassign *stmt)
         return false;
       }
 
-    case WIDEN_PLUS_EXPR:
-    case WIDEN_MINUS_EXPR:
     case PLUS_EXPR:
     case MINUS_EXPR:
       {
@@ -4139,10 +4137,6 @@ verify_gimple_assign_binary (gassign *stmt)
         return false;
       }
 
-    case VEC_WIDEN_MINUS_HI_EXPR:
-    case VEC_WIDEN_MINUS_LO_EXPR:
-    case VEC_WIDEN_PLUS_HI_EXPR:
-    case VEC_WIDEN_PLUS_LO_EXPR:
     case VEC_WIDEN_MULT_HI_EXPR:
     case VEC_WIDEN_MULT_LO_EXPR:
     case VEC_WIDEN_MULT_EVEN_EXPR:
