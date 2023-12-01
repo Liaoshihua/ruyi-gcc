7ddc530bc417fc5abe8eb1b1f0919c4535b47f64
Florian Weimer <fweimer@redhat.com>
2023-12-01 08:10:13 +0100

    c: Turn -Wimplicit-function-declaration into a permerror
    In the future, it may make sense to avoid cascading errors from
    the implicit declaration, especially its assumed int return type.
    This change here only changes the kind of the diagnostic, not
    its wording or consequences.
    
    gcc/
    
    	* doc/invoke.texi (Warning Options): Document changes.
    
    gcc/c/
    
    	PR c/91092
    	PR c/96284
    	* c-decl.cc (implicit_decl_permerror): Rename from
    	implicit_decl_warning.  Call permerror_opt instead of
    	pedwarn and warning_at.
    	(implicitly_declare): Adjust callers.
    
    gcc/testsuite/
    
    	* gcc.dg/permerror-default.c (implicit_function_declaration):
    	Expect the new permerror.
    	* gcc.dg/permerror-system.c: Likewise.
    	* c-c++-common/spellcheck-reserved.c (test, test_2): Expect
    	error instead of warning.
    	(f): Expect error instead of warning.
    	* gcc.dg/Wimplicit-function-declaration-c99.c: Compile with
    	-fpermissive due to expected warning.
    	* gcc.dg/Wimplicit-function-declaration-c99-2.c: New test.
    	Copied from gcc.dg/Wimplicit-function-declaration-c99.c.
    	Expect error.
    	* gcc.dg/missing-header-fixit-1.c: Compile with -fpermissive
    	due to expect error.
    	* gcc.dg/missing-header-fixit-1a.c: New test.  Copied from
    	gcc.dg/missing-header-fixit-1.c, but expect error.
    	* gcc.dg/missing-header-fixit-2.c: Compile with -fpermissive
    	due to expect error.
    	* gcc.dg/missing-header-fixit-2a.c: New test.  Copied from
    	gcc.dg/missing-header-fixit-2.c, but expect error.
    	* gcc.dg/missing-header-fixit-4.c: Compile with -fpermissive
    	due to expect error.
    	* gcc.dg/missing-header-fixit-4a.c: New test.  Copied from
    	gcc.dg/missing-header-fixit-4.c, but expect error.
    	* gcc.dg/missing-header-fixit-5.c: Compile with -fpermissive
    	due to expect error.
    	* gcc.dg/missing-header-fixit-5a.c: New test.  Copied from
    	gcc.dg/missing-header-fixit-5.c, but expect error.
    	* gcc.dg/pr61852.c: Expect implicit-function-declaration
    	error instead of warning.
    	* gcc.dg/spellcheck-identifiers-2.c: Compile with
    	-fpermissive due to expected warnings.
    	* gcc.dg/spellcheck-identifiers-2a.c: New test.  Copied
    	from gcc.dg/spellcheck-identifiers-2a.c.  Expect errors.
    	* gcc.dg/spellcheck-identifiers-3.c: Compile with
    	-fpermissive due to expected warnings.
    	* gcc.dg/spellcheck-identifiers-3a.c: New test.  Copied
    	from gcc.dg/spellcheck-identifiers-2a.c.  Expect errors.
    	* gcc.dg/spellcheck-identifiers-4.c: Compile with
    	-fpermissive due to expected warnings.
    	* gcc.dg/spellcheck-identifiers-4a.c: New test.  Copied
    	from gcc.dg/spellcheck-identifiers-2a.c.  Expect error.
    	* gcc.dg/spellcheck-identifiers.c: Compile with
    	-fpermissive due to expected warnings.
    	* gcc.dg/spellcheck-identifiers-1a.c: New test.  Copied
    	from gcc.dg/spellcheck-identifiers.c.  Expect errors.
    	* gcc.target/aarch64/sve/acle/general-c/ld1sh_gather_1.c (f1):
    	Expect error.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_index_1.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_index_restricted_1.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_1.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_2.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_3.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_4.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_5.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_restricted_1.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_restricted_2.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_restricted_3.c:
    	(f1): Likewise.
    	* gcc.target/aarch64/sve/acle/general-c/load_ext_gather_offset_restricted_4.c:
    	(f1): Likewise.

--- gcc/testsuite/gcc.dg/missing-header-fixit-5a.c
+++ gcc/testsuite/gcc.dg/missing-header-fixit-5a.c
@@ -3,32 +3,38 @@
    Rely on -Wimplicit-function-declaration for fixit hints, not on
    -Wbuiltin-declaration-mismatch (which misses abs, isdigit, putchar).  */
 
-/* { dg-options "-fpermissive -fdiagnostics-show-caret -fdiagnostics-show-line-numbers -Wimplicit-function-declaration -Wno-builtin-declaration-mismatch" } */
+/* { dg-options "-fdiagnostics-show-caret -fdiagnostics-show-line-numbers -Wimplicit-function-declaration -Wno-builtin-declaration-mismatch" } */
 
 int
 foo (char *m, int i)
 {
-  if (isdigit (m[0])) /* { dg-warning "implicit declaration of function" } */
+  if (isdigit (m[0])) /* { dg-error "implicit declaration of function" } */
   /* { dg-begin-multiline-output "" }
    11 |   if (isdigit (m[0]))
       |       ^~~~~~~
+     { dg-end-multiline-output "" } */
+  /* { dg-begin-multiline-output "" }
   +++ |+#include <ctype.h>
     1 | 
      { dg-end-multiline-output "" } */
     {
-      return abs (i); /* { dg-warning "implicit declaration of function" } */
+      return abs (i); /* { dg-error "implicit declaration of function" } */
   /* { dg-begin-multiline-output "" }
-   19 |       return abs (i);
+   21 |       return abs (i);
       |              ^~~
+     { dg-end-multiline-output "" } */
+  /* { dg-begin-multiline-output "" }
   +++ |+#include <stdlib.h>
     1 | 
      { dg-end-multiline-output "" } */
     }
   else
-    putchar (m[0]); /* { dg-warning "implicit declaration of function" } */
+    putchar (m[0]); /* { dg-error "implicit declaration of function" } */
   /* { dg-begin-multiline-output "" }
-   28 |     putchar (m[0]);
+   32 |     putchar (m[0]);
       |     ^~~~~~~
+     { dg-end-multiline-output "" } */
+  /* { dg-begin-multiline-output "" }
   +++ |+#include <stdio.h>
     1 | 
      { dg-end-multiline-output "" } */
