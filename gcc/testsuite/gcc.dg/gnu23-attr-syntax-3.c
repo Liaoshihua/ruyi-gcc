13caa06d5023267810b197b2dffbeb2b5107fb96
Florian Weimer <fweimer@redhat.com>
2023-12-01 08:10:13 +0100

    c: Turn -Wimplicit-int into a permerror
    Most of these new permerrors are currently not diagnosed in system
    headers.
    
    gcc/
    
    	PR c/91093
    	PR c/96284
    	* doc/invoke.texi (Warning Options): Document changes.
    
    gcc/c/
    
    	* c-decl.cc (warn_defaults_to): Remove.
    	(grok_declarator, start_function): Call permerror_opt
    	instead of warn_defaults_to.
    	(store_parm_decls_oldstyle): Call permerror_opt for
    	OPT_Wimplicit_int.
    
    gcc/testsuite/
    
    	* gcc.dg/permerror-default.c (implicit_int_1, implicit_int_2)
    	(implicit_int_3, implicit_int_4): Expect new permerror.
    	* gcc.dg/permerror-system.c: Expect a single new permerror.
    	* gcc.dg/Wimplicit-int-1.c: Compile with -fpermissive due to
    	expected warning.
    	* gcc.dg/Wimplicit-int-4.c: Likewise.
    	* gcc.dg/Wimplicit-int-1a.c: New test.  Copied from
    	gcc.dg/Wimplicit-int-1.c, but expect errors.
    	* gcc.dg/Wimplicit-int-4a.c: New test.  Copied from
    	gcc.dg/Wimplicit-int-4.c, but expect errors.
    	* gcc.dg/gnu23-attr-syntax-2.c: Compile with -fpermissive
    	due to expected implicit-int error.
    	* gcc.dg/gnu23-attr-syntax-3.c: New test.  Copied from
    	gcc.dg/gnu23-attr-syntax-2.c, but expect an error.
    	* gcc.dg/pr105635.c: Build with -fpermissive due to implicit
    	int.
    	* gcc.dg/pr105635-2.c: New test.  Copied from
    	gcc.dg/pr105635.c.  Expect implicit int error.
    	* gcc.dg/noncompile/pr79758.c: Build with -fpermissive due to
    	implicit int.
    	* gcc.dg/noncompile/pr79758-2.c: New test.  Copied from
    	gcc.dg/noncompile/pr79758.c.  Expect implicit int error.

--- gcc/testsuite/gcc.dg/gnu23-attr-syntax-3.c
+++ gcc/testsuite/gcc.dg/gnu23-attr-syntax-3.c
@@ -1,15 +1,16 @@
-/* Test C2x attribute syntax.  Invalid uses of attributes with GNU C
-   features.  */
+/* Test C23 attribute syntax.  Invalid uses of attributes with GNU C
+   features.  Non-permissive variant.  */
 /* { dg-do compile } */
-/* { dg-options "-fpermissive -std=gnu23 -w" } */
+/* { dg-options "-std=gnu23 -w" } */
 
 /* Attributes cannot be used as prefix attributes on old-style
    parameter declarations or on function declarators with identifier
-   lists (removed from C2x).  */
+   lists (removed from C23).  */
 
 void (*f(a, b) [[]])() int a, b; { } /* { dg-error "expected" } */
 
 void f(x, y) int x; [[]] int y; { } /* { dg-error "expected" } */
+/* { dg-error "type of 'y' defaults to 'int'" "" { target *-*-* } .-1 } */
 
 /* Nonempty attributes cannot be used as postfix attributes with
    __auto_type.  */
