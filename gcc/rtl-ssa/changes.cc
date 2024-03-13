d1c9b3369b4b378f0bb9fbecb3ca1447b908d8c8
Liao Shihua <shihua@iscas.ac.cn>
2024-03-13 22:26:04 +0800

    fix changes.cc build error

--- gcc/rtl-ssa/changes.cc
+++ gcc/rtl-ssa/changes.cc
@@ -631,12 +631,9 @@ function_info::finalize_new_accesses (insn_change &change, insn_info *pos)
 }
 
 // Copy information from CHANGE to its underlying insn_info, given that
-// the insn_info has already been placed appropriately.  NEW_SETS contains the
-// new set_infos that are being added as part of this change (as opposed to
-// being moved or repurposed from existing instructions).
+// the insn_info has already been placed appropriately.
 void
-function_info::apply_changes_to_insn (insn_change &change,
-				      hash_set<def_info *> &new_sets)
+function_info::apply_changes_to_insn (insn_change &change)
 {
   insn_info *insn = change.insn ();
   if (change.is_deletion ())
@@ -648,11 +645,10 @@ function_info::apply_changes_to_insn (insn_change &change,
   // Copy the cost.
   insn->set_cost (change.new_cost);
 
-  // Add all clobbers and newly-created sets.  Existing sets and call
-  // clobbers never move relative to other definitions, so are OK as-is.
+  // Add all clobbers.  Sets and call clobbers never move relative to
+  // other definitions, so are OK as-is.
   for (def_info *def : change.new_defs)
-    if ((is_a<clobber_info *> (def) && !def->is_call_clobber ())
-	|| (is_a<set_info *> (def) && new_sets.contains (def)))
+    if (is_a<clobber_info *> (def) && !def->is_call_clobber ())
       add_def (def);
 
   // Add all uses, now that their position is final.
@@ -786,27 +782,6 @@ function_info::change_insns (array_slice<insn_change *> changes)
 				 placeholder ? placeholder : insn);
 	}
       placeholders[i] = placeholder;
-    }
-
-  // We need to keep track of newly-added sets as these need adding to
-  // the def chain later.
-  hash_set<def_info *> new_sets;
-
-  // Finalize the new list of accesses for each change.  Don't install them yet,
-  // so that we still have access to the old lists below.
-  //
-  // Note that we do this forwards instead of in the backwards loop above so
-  // that any new defs being inserted are processed before new uses of those
-  // defs, so that the (initially) temporary uses referring to temporary defs
-  // can be easily updated to become permanent uses referring to permanent defs.
-  for (unsigned i = 0; i < changes.size (); i++)
-    {
-      insn_change &change = *changes[i];
-      insn_info *placeholder = placeholders[i];
-      if (!change.is_deletion ())
-	finalize_new_accesses (change,
-			       placeholder ? placeholder : change.insn (),
-			       new_sets);
     }
 
   // Remove all definitions that are no longer needed.  After the above,
